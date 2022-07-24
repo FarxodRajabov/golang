8675081b3bb04d663487225771da42918206200c
Farxod <96386942+FarxodRajabov@users.noreply.github.com>
2022-07-24 12:42:27 +0500

    type stract

--- lesson_3/main.go
+++ lesson_3/main.go
@@ -2,17 +2,38 @@ package main
 
 import "fmt"
 
+type Point struct {
+	X int
+	Y int
+}
+
 func main() {
 	pointers()
+	structs()
+}
+
+func structs() {
+	p := Point{
+		X: 0,
+		Y: 2,
+	}
+	fmt.Println(p)
 }
 
 func pointers() {
-	a := "Hello World"
-	b := 42
+	// a := "Hello World"
+	// b := 42
 
-	fmt.Println(a)
-	fmt.Println(b)
+	// fmt.Println(a)
+	// fmt.Println(b)
 
-	p := &a
-	fmt.Println(p)
+	// p := &a
+	// fmt.Println(p)
+	// fmt.Println(*p)
+	// *p = "GOGOGO"
+	// fmt.Println(*p)
+	// g := &b
+	// *g = *g / 2
+	// fmt.Println(*g)
+
 }
