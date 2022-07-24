891a935c716aaad74f9267e2caa4dc48a0e1cc36
Farxod <96386942+FarxodRajabov@users.noreply.github.com>
2022-07-24 11:57:25 +0500

    pointers

--- lesson_3/main.go
+++ lesson_3/main.go
@@ -1,1 +1,18 @@
 package main
+
+import "fmt"
+
+func main() {
+	pointers()
+}
+
+func pointers() {
+	a := "Hello World"
+	b := 42
+
+	fmt.Println(a)
+	fmt.Println(b)
+
+	p := &a
+	fmt.Println(p)
+}
