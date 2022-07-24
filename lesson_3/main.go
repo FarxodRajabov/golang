18886dd15f05677f2a8b7cba6a9036b27eed1df3
Farxod <96386942+FarxodRajabov@users.noreply.github.com>
2022-07-24 17:10:47 +0500

    type Point stract

--- lesson_3/main.go
+++ lesson_3/main.go
@@ -5,19 +5,37 @@ import "fmt"
 type Point struct {
 	X int
 	Y int
+	S string
 }
 
 func main() {
-	pointers()
-	structs()
+	// pointers()
+	// structs()
+
+	p1 := Point{
+		X: 2,
+		Y: 3,
+		S: "Hello",
+	}
+	// p1.method()
+	p2 := &p1
+	p2.method()
+
+}
+func (p Point) method() {
+	fmt.Println(p.X)
+	fmt.Println(p.Y)
+	fmt.Println(p.S)
 }
 
 func structs() {
-	p := Point{
-		X: 0,
-		Y: 2,
-	}
-	fmt.Println(p)
+	// p := Point{
+	// 	X: 0,
+	// 	Y: 2,
+	// 	S: "Hello",
+	// }
+	// fmt.Println(p)
+	// fmt.Println(p.X)
 }
 
 func pointers() {
