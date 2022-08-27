1c4fe795ad48a5b55367bd44cf255d8331f8225b
Farxod <96386942+FarxodRajabov@users.noreply.github.com>
2022-08-27 21:04:54 +0500

    gorutine

--- lesson_9/main.go
+++ lesson_9/main.go
@@ -1,11 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+// func main() {
+// 	ch := make(chan int)
+// 	go sayHello(ch)
+
+// 	for i := range ch {
+// 		fmt.Println(i)
+// 	}
+
+// }
+
+// func say(world string) {
+// 	fmt.Println(world)
+// }
+
+// func sayHello(exit chan int) {
+// 	for i := 0; i < 5; i++ {
+// 		time.Sleep(100 * time.Millisecond)
+// 		exit <- i
+// 	}
+// 	close(exit)
+// }
 
 func main() {
-	go say("hello")
+	data := make(chan int)
+	exit := make(chan int)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			fmt.Println(<-data)
+		}
+		exit <- 0
+	}()
+	selectOne(data, exit)
 }
 
-func say(world string) {
-	fmt.Println(world)
+func selectOne(data, exit chan int) {
+	x := 0
+	for {
+		select {
+		case data <- x:
+			x += 1
+		case <-exit:
+			fmt.Println("exit")
+			return
+			// default:
+			// 	fmt.Println("waiting...")
+			// 	time.Sleep(50 * time.Millisecond)
+		}
+	}
 }
