c86985bc7f4bfe42291cea5eca96da1a62024c15
Farxod <96386942+FarxodRajabov@users.noreply.github.com>
2022-09-03 18:37:03 +0500

    wokr with file

---
+++ lesson_12/main.go
@@ -0,0 +1,29 @@
+package main
+
+// func main() {
+// 	writeFile()
+// }
+// func appendToFile() {
+// 	f, err := os.OpenFile("test.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	defer f.Close()
+// }
+
+// func writeFile() {
+// 	data := []byte("My name is Farhod")
+// 	err := ioutil.WriteFile("test.txt", data, 0777)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// }
+
+// func readFile() {
+// 	data, err := ioutil.ReadFile("test.txt")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	fmt.Println(string(data))
+// }
+
